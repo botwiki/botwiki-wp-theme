f5fed4afe8d97361e8ecdb1805f02e90706484f4
Stefan Bohacek <stefan@fourtonfish.com>
2018-03-09 07:02:47 -0500

    Added CSS file versioning.

--- functions.php
+++ functions.php
@@ -43,25 +43,25 @@ if (!isset($content_width)){
   $content_width = 980;
 }
 
-if (function_exists('add_theme_support')){
+if ( function_exists( 'add_theme_support' ) ){
  
-  add_theme_support('menus');
+  add_theme_support( 'menus' );
 
  
-  add_theme_support('post-thumbnails');
-  add_image_size('large', 700, '', true);
-  add_image_size('medium', 250, '', true);
-  add_image_size('small', 120, '', true);
-  add_image_size('custom-size', 700, 200, true);
+  add_theme_support( 'post-thumbnails' );
+  add_image_size( 'large', 700, '', true);
+  add_image_size( 'medium', 250, '', true);
+  add_image_size( 'small', 120, '', true);
+  add_image_size( 'custom-size', 700, 200, true);
 
  
-  /*add_theme_support('custom-background', array(
+  /*add_theme_support( 'custom-background', array(
 'default-color' => 'FFF',
 'default-image' => get_template_directory_uri() . '/img/bg.jpg'
   ));*/
 
  
-  /*add_theme_support('custom-header', array(
+  /*add_theme_support( 'custom-header', array(
 'default-image'			=> get_template_directory_uri() . '/img/headers/default.jpg',
 'header-text'			=> false,
 'default-text-color'		=> '000',
@@ -74,8 +74,8 @@ if (function_exists('add_theme_support')){
   ));*/
 
  
-  add_theme_support('automatic-feed-links');
-  load_theme_textdomain('botwiki', get_template_directory() . '/languages');
+  add_theme_support( 'automatic-feed-links' );
+  load_theme_textdomain( 'botwiki', get_template_directory() . '/languages' );
 }
 
 /*------------------------------------*\
@@ -96,17 +96,17 @@ Functions
 
 function botwiki_site_nav(){
   $frontpage_id = get_option( 'page_on_front' );
-  echo get_post_field('post_content', $frontpage_id);
+  echo get_post_field( 'post_content', $frontpage_id);
 
 }
 
 // Load HTML5 Blank scripts (header.php)
 function html5blank_header_scripts(){
   if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-    wp_register_script('bootstrap-js', get_template_directory_uri() . '/libs/bootstrap/bootstrap.min.js', array('jquery'));
-    wp_enqueue_script('bootstrap-js');
-    wp_register_script('scripts', get_template_directory_uri() . '/js/scripts.min.js', array('jquery'), '1.0.0');
-    wp_enqueue_script('scripts');
+    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/libs/bootstrap/bootstrap.min.js', array( 'jquery' ));
+    wp_enqueue_script( 'bootstrap-js' );
+    wp_register_script( 'scripts', get_template_directory_uri() . '/js/scripts.min.js', array( 'jquery' ), '1.0.0' );
+    wp_enqueue_script( 'scripts' );
   }
 }
 
@@ -120,38 +120,41 @@ function load_twitter_js(){
 function load_highlight_js(){
   global $post;
   if ( strpos( get_post_field( 'post_content', $post->ID ), '<code>' ) !== false ){
-    wp_register_style('highlight-monokai-sublime', get_template_directory_uri() . '/libs/highlight.js/styles/monokai-sublime.css');
-    wp_enqueue_style('highlight-monokai-sublime');
+    wp_register_style( 'highlight-monokai-sublime', get_template_directory_uri() . '/libs/highlight.js/styles/monokai-sublime.css' );
+    wp_enqueue_style( 'highlight-monokai-sublime' );
 
-    wp_register_script('highlight', get_template_directory_uri() . '/libs/highlight.js/highlight.pack.js', array('jquery'));
-    wp_enqueue_script('highlight');
+    wp_register_script( 'highlight', get_template_directory_uri() . '/libs/highlight.js/highlight.pack.js', array( 'jquery' ));
+    wp_enqueue_script( 'highlight' );
 
-    wp_add_inline_script('highlight', "jQuery(function(){if (hljs){hljs.initHighlighting.called = false;hljs.initHighlighting();}});", 'after');
+    wp_add_inline_script( 'highlight', "jQuery(function(){if (hljs){hljs.initHighlighting.called = false;hljs.initHighlighting();}});", 'after' );
   }
 }
 
 function load_styles(){
-  wp_register_style('bootstrap-styles', get_template_directory_uri() . '/libs/bootstrap/bootstrap.min.css', array(), '1.0', 'all');
-  wp_enqueue_style('bootstrap-styles');
-  wp_register_style('styles', get_template_directory_uri() . '/css/styles.min.css', array(), '1.0', 'all');
-  wp_enqueue_style('styles');
+  $css_file_path = get_stylesheet_directory() . '/css/styles.min.css';
+
+  wp_register_style( 'bootstrap-styles', get_template_directory_uri() . '/libs/bootstrap/bootstrap.min.css', array(), filemtime( $css_file_path ), 'all' );
+  wp_enqueue_style( 'bootstrap-styles' );
+  wp_register_style( 'styles', get_template_directory_uri() . '/css/styles.min.css', array(), '1.0', 'all' );
+  wp_enqueue_style( 'styles' );
 }
 
 function load_admin_styles(){
-  wp_register_style('admin-styles', get_template_directory_uri() . '/admin-css/styles.min.css', array(), '1.0', 'all');
-  wp_enqueue_style('admin-styles');
+  $css_file_path = get_stylesheet_directory() . '/admin-css/styles.min.css';
+  wp_register_style( 'admin-styles', get_template_directory_uri() . '/admin-css/styles.min.css', array(), filemtime( $css_file_path ), 'all' );
+  wp_enqueue_style( 'admin-styles' );
 }
 
 function register_site_menu(){
   register_nav_menus(array(
-    'header-menu' => __('Header Menu', 'botwiki'),
-    'sidebar-menu' => __('Sidebar Menu', 'botwiki'),
-    'extra-menu' => __('Extra Menu', 'botwiki')
+    'header-menu' => __( 'Header Menu', 'botwiki' ),
+    'sidebar-menu' => __( 'Sidebar Menu', 'botwiki' ),
+    'extra-menu' => __( 'Extra Menu', 'botwiki' )
   ));
 }
 
 // Remove the <div> surrounding the dynamic navigation to cleanup markup
-function my_wp_nav_menu_args($args = ''){
+function my_wp_nav_menu_args($args = '' ){
   $args['container'] = false;
   return $args;
 }
@@ -163,14 +166,14 @@ function my_css_attributes_filter($var){
 
 // Remove invalid rel attribute values in the categorylist
 function remove_category_rel_from_category_list($thelist){
-  return str_replace('rel="category tag"', 'rel="tag"', $thelist);
+  return str_replace( 'rel="category tag"', 'rel="tag"', $thelist);
 }
 
 // Add page slug to body class, love this - Credit: Starkers Wordpress Theme
 function add_slug_to_body_class($classes){
   global $post;
   if (is_home()) {
-    $key = array_search('blog', $classes);
+    $key = array_search( 'blog', $classes);
     if ($key > -1) {
         unset($classes[$key]);
     }
@@ -184,11 +187,11 @@ function add_slug_to_body_class($classes){
 }
 
 // If Dynamic Sidebar Exists
-if (function_exists('register_sidebar')){
+if (function_exists( 'register_sidebar' )){
  
   register_sidebar(array(
-    'name' => __('Widget Area 1', 'botwiki'),
-    'description' => __('Description for this widget-area...', 'botwiki'),
+    'name' => __( 'Widget Area 1', 'botwiki' ),
+    'description' => __( 'Description for this widget-area...', 'botwiki' ),
     'id' => 'widget-area-1',
     'before_widget' => '<div id="%1$s" class="%2$s">',
     'after_widget' => '</div>',
@@ -198,8 +201,8 @@ if (function_exists('register_sidebar')){
 
  
   register_sidebar(array(
-    'name' => __('Widget Area 2', 'botwiki'),
-    'description' => __('Description for this widget-area...', 'botwiki'),
+    'name' => __( 'Widget Area 2', 'botwiki' ),
+    'description' => __( 'Description for this widget-area...', 'botwiki' ),
     'id' => 'widget-area-2',
     'before_widget' => '<div id="%1$s" class="%2$s">',
     'after_widget' => '</div>',
@@ -211,7 +214,7 @@ if (function_exists('register_sidebar')){
 // Remove wp_head() injected Recent Comment styles
 function my_remove_recent_comments_style(){
   global $wp_widget_factory;
-  remove_action('wp_head', array(
+  remove_action( 'wp_head', array(
     $wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
     'recent_comments_style'
   ));
@@ -224,7 +227,7 @@ function botwiki_site_pagination(){
   echo paginate_links(array(
     'base' => str_replace($big, '%#%', get_pagenum_link($big)),
     'format' => '?paged=%#%',
-    'current' => max(1, get_query_var('paged')),
+    'current' => max(1, get_query_var( 'paged' )),
     'total' => $wp_query->max_num_pages
   ));
 }
@@ -245,24 +248,24 @@ function html5wp_index($length){
   return 20;
 }
 
-// Create 40 Word Callback for Custom Post Excerpts, call using bw_excerpt('html5wp_custom_post');
+// Create 40 Word Callback for Custom Post Excerpts, call using bw_excerpt( 'html5wp_custom_post' );
 function html5wp_custom_post($length){
   return 40;
 }
 
 // Create the Custom Excerpts callback
-function bw_excerpt($post_data, $length_callback = '', $more_callback = ''){
+function bw_excerpt($post_data, $length_callback = '', $more_callback = '' ){
 
   global $post;
   if (function_exists($length_callback)) {
-    add_filter('excerpt_length', $length_callback);
+    add_filter( 'excerpt_length', $length_callback);
   }
   if (function_exists($more_callback)) {
-    add_filter('excerpt_more', $more_callback);
+    add_filter( 'excerpt_more', $more_callback);
   }
   $output = get_the_excerpt();
-  $output = apply_filters('wptexturize', $output);
-  $output = apply_filters('convert_chars', $output);
+  $output = apply_filters( 'wptexturize', $output);
+  $output = apply_filters( 'convert_chars', $output);
   $output = '<p>' . $output . '</p>';
   echo $output;
 }
@@ -280,12 +283,12 @@ function html5_blank_view_article($more){
 
 // Remove 'text/css' from our enqueued stylesheet
 function html5_style_remove($tag){
-  return preg_replace('~\s+type=["\'][^"\']++["\']~', '', $tag);
+  return preg_replace( '~\s+type=["\'][^"\']++["\']~', '', $tag);
 }
 
 // Remove thumbnail width and height dimensions that prevent fluid images in the_thumbnail
 function remove_thumbnail_dimensions( $html ){
-  $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
+  $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html);
   return $html;
 }
 
@@ -301,53 +304,53 @@ Actions + Filters + ShortCodes
 \*------------------------------------*/
 
 // Add Actions
-add_action('init', 'html5blank_header_scripts');
-add_action('wp_enqueue_scripts', 'load_styles');
-add_action('wp_enqueue_scripts', 'load_twitter_js');
-add_action('wp_enqueue_scripts', 'load_highlight_js');
-add_action('admin_enqueue_scripts', 'load_admin_styles');
-add_action('init', 'register_site_menu');
-add_action('widgets_init', 'my_remove_recent_comments_style');
-add_action('init', 'botwiki_site_pagination');
+add_action( 'init', 'html5blank_header_scripts' );
+add_action( 'wp_enqueue_scripts', 'load_styles' );
+add_action( 'wp_enqueue_scripts', 'load_twitter_js' );
+add_action( 'wp_enqueue_scripts', 'load_highlight_js' );
+add_action( 'admin_enqueue_scripts', 'load_admin_styles' );
+add_action( 'init', 'register_site_menu' );
+add_action( 'widgets_init', 'my_remove_recent_comments_style' );
+add_action( 'init', 'botwiki_site_pagination' );
 
 // Remove Actions
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'rel_canonical');
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action( 'wp_head', 'feed_links_extra', 3);
+remove_action( 'wp_head', 'feed_links', 2);
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0);
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0);
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action( 'wp_head', 'rel_canonical' );
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0);
 
 // Add Filters
-add_filter('avatar_defaults', 'html5blankgravatar');
-add_filter('body_class', 'add_slug_to_body_class');
-add_filter('widget_text', 'do_shortcode');
-add_filter('widget_text', 'shortcode_unautop');
-add_filter('wp_nav_menu_args', 'my_wp_nav_menu_args');
-// add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);by default)
-// add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);default)
-// add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
-add_filter('the_category', 'remove_category_rel_from_category_list');
-add_filter('the_excerpt', 'shortcode_unautop');
-add_filter('the_excerpt', 'do_shortcode');
-add_filter('excerpt_more', 'html5_blank_view_article');
-// add_filter('show_admin_bar', 'remove_admin_bar');
-add_filter('style_loader_tag', 'html5_style_remove');
-add_filter('post_thumbnail_html', 'remove_thumbnail_dimensions', 10);
-add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10);
+add_filter( 'avatar_defaults', 'html5blankgravatar' );
+add_filter( 'body_class', 'add_slug_to_body_class' );
+add_filter( 'widget_text', 'do_shortcode' );
+add_filter( 'widget_text', 'shortcode_unautop' );
+add_filter( 'wp_nav_menu_args', 'my_wp_nav_menu_args' );
+// add_filter( 'nav_menu_css_class', 'my_css_attributes_filter', 100, 1);by default)
+// add_filter( 'nav_menu_item_id', 'my_css_attributes_filter', 100, 1);default)
+// add_filter( 'page_css_class', 'my_css_attributes_filter', 100, 1);
+add_filter( 'the_category', 'remove_category_rel_from_category_list' );
+add_filter( 'the_excerpt', 'shortcode_unautop' );
+add_filter( 'the_excerpt', 'do_shortcode' );
+add_filter( 'excerpt_more', 'html5_blank_view_article' );
+// add_filter( 'show_admin_bar', 'remove_admin_bar' );
+add_filter( 'style_loader_tag', 'html5_style_remove' );
+add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10);
+add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10);
 
 // Remove Filters
-remove_filter('the_excerpt', 'wpautop');
+remove_filter( 'the_excerpt', 'wpautop' );
 
 // Shortcodes
-add_shortcode('html5_shortcode_demo', 'html5_shortcode_demo');
-add_shortcode('html5_shortcode_demo_2', 'html5_shortcode_demo_2');
+add_shortcode( 'html5_shortcode_demo', 'html5_shortcode_demo' );
+add_shortcode( 'html5_shortcode_demo_2', 'html5_shortcode_demo_2' );
 
 // Shortcodes above would be nested like this -
 // [html5_shortcode_demo] [html5_shortcode_demo_2] Here's the page title! [/html5_shortcode_demo_2] [/html5_shortcode_demo]
@@ -372,8 +375,8 @@ function html5_shortcode_demo_2($atts, $content = null){
 
 // Allow <p> tags in author biographical bio.
 
-remove_filter( 'pre_user_description',  'wp_filter_kses');
-add_filter(  'pre_user_description',  'wp_filter_post_kses');
+remove_filter( 'pre_user_description',  'wp_filter_kses' );
+add_filter(  'pre_user_description',  'wp_filter_post_kses' );
 
 add_post_type_support( 'page', 'excerpt' );
 
