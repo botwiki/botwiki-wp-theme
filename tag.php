8a7effb101dc404382844014e7dc9a07a710b012
Stefan Bohacek <stefan@fourtonfish.com>
2018-03-11 21:25:24 -0400

    Show tags on tag page as links.

--- tag.php
+++ tag.php
@@ -1,7 +1,18 @@
-<?php get_header(); ?>
+<?php
+  get_header();
+  global $wp_query;
+  $tags = preg_split( "/(\+|,)/", $wp_query->query['tag'] );
+
+  $tag_links = array();
+
+  foreach ($tags as $tag) {
+    $tag_links[] = '<a href="/tag/' . $tag . '">#' . $tag . '</a>';
+  }
+?>
+
   <main role="main" class="container-fluid m-0 p-0">
     <div class="container">
-      <h1><?php echo "Posts tagged #" . single_tag_title('', false); ?></h1>
+      <h1><?php echo "Posts tagged " . implode( ' ', $tag_links ); ?></h1>
 			<?php get_template_part('loop'); ?>
 			<?php get_template_part('pagination'); ?>
 		</div>
