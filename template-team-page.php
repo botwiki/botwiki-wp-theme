32ac254ac93aeadb0260ea80e8d3d751e62f6df3
Stefan Bohacek <stefan@fourtonfish.com>
2018-04-17 20:59:36 -0400

    Show team role on team page.

--- template-team-page.php
+++ template-team-page.php
@@ -60,6 +60,16 @@
                 $full_name = "{$first_name} {$last_name}";
             }
 
+            if ( current_user_can('administrator') ){  
+              $botwiki_team_role = get_the_author_meta('botwiki-team-role', $author_id);
+              if ( empty( $botwiki_team_role ) ){
+                $botwiki_team_role = "Botwiki team member";      
+              }
+            }
+            else{
+              $botwiki_team_role = "Botwiki contributor";    
+            }
+
             $website_url = esc_attr( get_the_author_meta( 'user_url', $author_id ) );
             $twitter_handle = str_replace('@', '', esc_attr( get_the_author_meta( 'twitter-handle', $author_id ) ) );
 
@@ -70,6 +80,12 @@
               </a>
               <div class="media-body">
                 <h3 id="<?php echo $username; ?>" class="mt-0 mb-1"><?php echo $nickname; ?><a class="pilcrow" href="#<?php echo $username; ?>">¶</a></h3>
+                <div class="card mt-4 mb-4">
+                  <div class="card-body">
+                    <p class="card-text font-weight-bold"><?php echo $botwiki_team_role; ?></p>
+                  </div>
+                </div>
+
                 <div class="p-note">
                   <?php echo $description; ?>
                 </div>
