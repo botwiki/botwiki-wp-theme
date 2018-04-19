8a0a0c9c38bc45c9fd4c5e49792f5b252f313852
Stefan Bohacek <stefan@fourtonfish.com>
2018-04-19 07:00:27 -0400

    Added images to RSS feed.

--- includes/rss-feed.php
+++ includes/rss-feed.php
@@ -1,16 +1,26 @@
 <?php
 
-/* Add custom post types to the RSS feed. */
+/* Customizing the RSS feed. */
 
 class Custom_Post_Type_RSS {
   public function __construct() {
     add_filter( 'request', array( $this, 'rss_feed' ) );
+    add_filter( 'the_excerpt_rss', array( $this, 'rss_feed_image' ) );
+    add_filter( 'the_content_feed', array( $this, 'rss_feed_image' ) );
   }
 
   public function rss_feed( $query_var ) { 
     if ( isset( $query_var['feed'] ) )
         $query_var['post_type'] = array( 'post', 'bot', 'resource' );
     return $query_var;
+  }
+
+  public function rss_feed_image( $content ) { 
+    global $post;
+    if ( has_post_thumbnail( $post->ID ) ){
+      $content = '<div>' . get_the_post_thumbnail( $post->ID, 'full', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
+    }
+    return $content;
   }
 }
 
