ff0c5f2796bc7efe6e2fdec6c82126d5912b38bc
Stefan Bohacek <stefan@fourtonfish.com>
2018-04-18 23:01:05 -0400

    Debugging.

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ require 'includes/show-post-id.php';
 require 'includes/card-title.php';
 require 'includes/add-tags-to-pages.php';
 require 'includes/extra-user-fields.php';
-// require 'includes/contributor-hide-others-posts.php';
+require 'includes/contributor-hide-others-posts.php';
 require 'includes/new-post-instructions.php';
 
 
