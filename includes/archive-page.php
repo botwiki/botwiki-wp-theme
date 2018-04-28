9e9c3a8df0836a3290261a4a22c0786e6704d530
Stefan Bohacek <stefan@fourtonfish.com>
2018-04-28 08:25:58 -0400

    Use https version of the Internet Archive URL.

--- includes/archive-page.php
+++ includes/archive-page.php
@@ -8,7 +8,7 @@ class Archive_This_Page {
   public function archive_page( $post_id, $post, $update ) {
     if ( !empty($post) && $post->post_status === 'publish' ){
       $page_url = get_permalink( $post_id );
-      $internet_archive_url = 'http://web.archive.org/save/' . $page_url;
+      $internet_archive_url = 'https://web.archive.org/save/' . $page_url;
 
       $ch = curl_init(); 
       curl_setopt($ch, CURLOPT_URL, $internet_archive_url); 
