508543d800429ccafed8031089802ffc5a3ddcf8
Stefan Bohacek <stefan@fourtonfish.com>
2018-05-09 21:12:14 -0400

    Use 'orderby' => 'rand' WP Query to get a random bot.

--- includes/random-bot.php
+++ includes/random-bot.php
@@ -8,15 +8,13 @@ class Random_Bot_Redirect {
   public function show_random_bot( $content ) { 
     global $wp_query;
     if ($wp_query->query['name'] === 'random-bot'){
-      $all_bots = new WP_Query(array(
+      $random_bot = new WP_Query(array(
         'post_type' => 'bot',
         'post_status' => 'publish',
-        'posts_per_page' => -1,
-        'ignore_sticky_posts'=> true
+        'posts_per_page' => 1,
+        'orderby' => 'rand'
       ));
-      $random_bot = $all_bots->posts[array_rand($all_bots->posts)];
-      $random_bot_url = get_post_permalink($random_bot->ID);
-      wp_redirect($random_bot_url);
+      wp_redirect( get_post_permalink($random_bot->post->ID) );
     }
   }
 }
