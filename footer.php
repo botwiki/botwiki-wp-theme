f3dd4cf3e778be0fd28685e9c4f035a3af7d2c1a
Stefan Bohacek <stefan@fourtonfish.com>
2019-03-31 10:51:57 -0400

    Fixed footer spacing.

--- footer.php
+++ footer.php
@@ -24,7 +24,7 @@
 
       <!-- footer -->
       <footer class="footer mt-5 mb-0 background-fff" role="contentinfo">
-        <div class="container-fluid background-alternate pt-5 pb-5">
+        <div class="container-fluid background-alternate pt-5 pb-1">
           <div class="container share-prompt">
             <h3>Share this <?php echo (is_front_page() ? 'site' : 'page' );?></h3>
             <?php
@@ -79,7 +79,7 @@
           </div>
         </div>
  -->
-        <div class="container-fluid background-alternate pl-5 pr-5 pt-1">
+        <div class="container-fluid background-alternate pt-1 pb-5">
           <div class="container">
             <div class="row mt-5 mb-5">
               <div class="col-sm-12 col-md-4">
