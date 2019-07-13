9f3f3f8de0e151b37f1a8be133f05b29536d1f2b
Stefan Bohacek <stefan@fourtonfish.com>
2019-07-13 18:31:50 -0400

    Added log_this helper function.

--- functions.php
+++ functions.php
@@ -493,3 +493,40 @@ function enqueue_scripts_async( $tag, $handle, $src ) {
 }
 
 add_filter( 'script_loader_tag', 'enqueue_scripts_async', 10, 3 );
+
+/*
+    Helper dev functions.
+*/
+
+function log_this( $title, $data = false ){
+  if ( func_num_args( ) === 0 ){
+    return false;
+  }
+  elseif ( func_num_args( ) === 1 ){
+    $data = $title;
+    $title = "LOG";
+  }
+
+  $border_char = "/";
+  $border_length_bottom = 100;
+  $border_length_top = $border_length_bottom - strlen( $title ) - 2;
+
+  $border_length_top_left = floor( $border_length_top/2 );
+  $border_length_top_right = ceil( $border_length_top/2 );
+
+  $border_top_left = str_pad( "", $border_length_top_left, $border_char );
+  $border_top_right = str_pad( "", $border_length_top_right, $border_char );  
+
+  error_log( "\n\n" );
+  error_log( "$border_top_left $title $border_top_right" );
+
+  if ( is_array( $data ) || is_object( $data ) ){
+    error_log( print_r( $data, true ) );
+  }
+  else{
+    error_log( "" );
+    error_log( $data );
+    error_log( "" );
+  }
+  error_log( str_pad( "", $border_length_bottom, $border_char ) . "\n" );
+}
