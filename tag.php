2a6226ee8b5011ae645c81f64ae32ecd244451bb
Stefan Bohacek <stefan.bohacek@gmail.com>
2019-08-26 09:07:21 -0400

    When browsing the twitterbot tag, show link to browse Twitter bots.

--- tag.php
+++ tag.php
@@ -9,13 +9,18 @@
     $tag_links[] = '<a href="/tag/' . $tag . '">#' . $tag . '</a>';
   }
 ?>
-
   <main role="main" class="container-fluid m-0 p-0">
     <div class="container">
       <h1><?php echo "All posts tagged " . implode( ' ', $tag_links ); ?></h1>
       <?php if ( in_array( 'opensource', $tags ) ){ ?>
-        <a class="btn mb-3" href="/bot/?opensource=true">Browse opensource bots</a>
-      <?php } ?>      
+        <ul class="btn-list">
+          <li><a class="btn" href="/bot/?opensource=true/">Browse opensource bots</a></li>
+        </ul>
+      <?php } elseif ( in_array( 'twitterbot', $tags ) ) { ?>
+        <ul class="btn-list">
+          <li><a class="btn" href="/bots/twitterbots/">Browse Twitter bots</a></li>
+        </ul>
+      <?php } ?>
 			<?php get_template_part('loop'); ?>
 			<?php get_template_part('pagination'); ?>
 		</div>
