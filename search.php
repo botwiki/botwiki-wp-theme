07ee125e978bcde970aebff58185dec00e296a7d
Stefan Bohacek <stefan.bohacek@gmail.com>
2019-11-21 08:46:15 -0500

    Fixed list alignment on the search page.

--- search.php
+++ search.php
@@ -49,23 +49,25 @@
       if ($wp_query->found_posts > 0){ ?>      
       <h3>Not quite what you're looking for?</h3>
       <?php }?>
-      <ul>
-        <li>
-          <a title="Add your bot, tutorial, and other botmaking resources to Botwiki" href="<?php echo home_url(); ?>/contribute/">Contribute to Botwiki</a>
-          <ul>
-            <li><em>Please be patient while we review the submissions</em> <span title="Thank you!">🙇</span></li>
-          </ul>  
-        </li>
-        <li>
-          <a href="https://botmakers.org/">Ask in the Botmakers community</a>
-        </li>
-        <li>
-          <a href="mailto:<?php echo $helpers->get_admin_emails(); ?>">Send us an email</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/botwikidotorg">Find us on Twitter</a>
-        </li>
-      </ul>
+      <div class="container">
+        <ul>
+          <li>
+            <a title="Add your bot, tutorial, and other botmaking resources to Botwiki" href="<?php echo home_url(); ?>/contribute/">Contribute to Botwiki</a>
+            <ul>
+              <li><em>Please be patient while we review the submissions</em> <span title="Thank you!">🙇</span></li>
+            </ul>  
+          </li>
+          <li>
+            <a href="https://botmakers.org/">Ask in the Botmakers community</a>
+          </li>
+          <li>
+            <a href="mailto:<?php echo $helpers->get_admin_emails(); ?>">Send us an email</a>
+          </li>
+          <li>
+            <a href="https://twitter.com/botwikidotorg">Find us on Twitter</a>
+          </li>
+        </ul>
+      </div>
 		</div>
 	</main>
 <?php get_footer(); ?>
