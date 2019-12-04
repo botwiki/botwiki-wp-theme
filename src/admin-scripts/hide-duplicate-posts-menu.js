ee24b12d2177527e757a571f95dd89129bedc289
Stefan Bohacek <stefan.bohacek@gmail.com>
2019-12-04 10:23:11 -0500

    Fixed script loading order issue.

--- src/admin-scripts/hide-duplicate-posts-menu.js
+++ src/admin-scripts/hide-duplicate-posts-menu.js
@@ -1,24 +1,25 @@
 ( function( $ ) {
-  'use strict';
+    'use strict';
+    $(window).load(function() {
+      function waitForElement(selector, callback) {
+        if ($(selector).length) {
+          callback();
+        } else {
+          var helpers = this;
 
-  function wait_for_el(selector, callback) {
-    if ($(selector).length) {
-      callback();
-    } else {
-      var helpers = this;
+          setTimeout(function() {
+            helpers.wait_for_el(selector, callback);
+          }, 100);
+        }
+      };
 
-      setTimeout(function() {
-        helpers.wait_for_el(selector, callback);
-      }, 100);
-    }
-  };
+      waitForElement('[id="menu-posts"]', function() {
+        /* Quick fix for duplicate Posts appearing in admin, after modifying default post slug. */
+        var $menu_posts = $('[id="menu-posts"]');
 
-  wait_for_el('[id="menu-posts"]', function() {
-    /* Quick fix for duplicate Posts appearing in admin, after modifying default post slug. */
-    var $menu_posts = $('[id="menu-posts"]');
-
-    if ($menu_posts.length > 1){
-      $menu_posts.first().hide();  
-    }
-  });
+        if ($menu_posts.length > 1){
+          $menu_posts.first().hide();  
+        }
+      });
+    });
 } )( jQuery );
