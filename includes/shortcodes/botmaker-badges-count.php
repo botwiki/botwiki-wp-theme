f66560f0240137d0bbdfce9b9d8d0cdf8ae87dc7
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-01-13 09:26:12 -0500

    Fixed Botmaker Badge count offset.

--- includes/shortcodes/botmaker-badges-count.php
+++ includes/shortcodes/botmaker-badges-count.php
@@ -7,7 +7,7 @@ class Botmaker_Badges_Count {
 
   public function get_botmaker_badges_count() {
     global $helpers;
-    $offset = 45;
+    $offset = 46;
 
     $args = array(
         'posts_per_page' => -1,
