0844817ba124f500df9ea2891218b001c99effd5
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-01-27 13:16:21 -0500

    Added new bot output layout style.

--- includes/post-types/bots.php
+++ includes/post-types/bots.php
@@ -79,6 +79,23 @@ class BotsPostType {
           $html .= '</div>';
         }
         $html .= '</div></div>';
+      } elseif ( $layout_style === 'layout-4' ){
+        $html .= '<div class="container mt-5 mb-5"><div class="row no-gutters">';
+
+        foreach ( $bot_output as $index => $bot_output_item ) {
+          $html .= '<div class="col-sm-12 col-md-4 p-1">';
+
+          if ( !empty( $bot_output_item['bot-output-image'] ) ){
+            $html .= '<a href="' . $bot_output_item['source-url'] . '">';
+            $html .= '<img class="bot-output bot-output-img lazy-load" data-src="' . $bot_output_item['bot-output-image']['url'] . '">';
+            $html .= '<noscript><img class="bot-output bot-output-img lazy-load" src="' . $bot_output_item['bot-output-image']['url'] . '"></noscript>';
+            $html .= '</a';
+          }
+          $html .= '</div>';
+        }
+        $html .= '</div></div>';
+
+
       } elseif ( $layout_style === 'layout-3' ){
         $bot_output_count_half = floor( $bot_output_count / 2 );
 
