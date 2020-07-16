4b59d5425f8e7be694e8be1934c9045b60681260
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-07-16 14:49:00 -0400

    Updated blog design.

--- loop-blog.php
+++ loop-blog.php
@@ -13,10 +13,10 @@
     ?>
     <div class="post-thumbnail post-thumbnail-<?php echo $page; ?>-<?php echo $index; ?>">
       <a href="<?php echo $link_url; ?>" title="<?php get_the_title(); ?>">
-        <div class="post-thumbnail-img" style="background-image:url('<?php
-          echo get_the_post_thumbnail_url( $post_thumbnail_id, 'medium_large' );
-        ?>');">
-        </div>
+        <img class="post-thumbnail-img lazy-load"
+             src="<?php echo get_the_post_thumbnail_url( $post_thumbnail_id, 'medium_large' ); ?>"
+             data-src="<?php echo get_the_post_thumbnail_url( $post_thumbnail_id, 'medium_large' ); ?>"
+        >
         <!-- post title -->
         <p class="post-thumbnail-title">
           <?php
@@ -24,9 +24,11 @@
 
           $post_date = get_the_time('F j, Y');
           $post_date_full = $post_date . ' ' . get_the_time('g:i a');
+          $post_author = get_the_author();
+
           $m = new \Moment\Moment($post_date);
           $post_date_ago = $m->fromNow()->getRelative();
-          echo " <span class='post-thumbnail-date'>$post_date_ago</span>";
+          echo " <span class='post-thumbnail-date'>$post_date_ago by $post_author</span>";
           ?>
         </p>
         <!-- /post title -->
@@ -44,22 +46,22 @@
 
         if ( $page === 1 && $index === 1 ){ ?>
           <div class="row">
-            <div class="col-sm-12 col-md-8 text-center m-0 p-0">
+            <div class="col-sm-12 col-md-8 text-center m-0 p-1">
               <?php render_post( $page, $index, $post->ID ); ?>
             </div>
         <?php } elseif ( $page !== 1 && $index === 1 ) { ?>
           <div class="row">
-              <div class="col-sm-12 col-md-4 text-center p-0 pb-3 m-0 p-0">
+              <div class="col-sm-12 col-md-4 text-center p-1 m-0">
                 <?php render_post( $page, $index, $post->ID ); ?>
               </div>
         <?php } elseif ( $page === 1 && $index === 2 ) { ?>
           <div class="col-sm-12 col-md-4">
             <div class="row">
-              <div class="col-sm-12 text-center m-0 p-0">
+              <div class="col-sm-12 text-center m-0 p-1">
                 <?php render_post( $page, $index, $post->ID ); ?>
               </div>
           <?php } elseif ( $page === 1 && $index === 3 ) { ?>
-              <div class="col-sm-12 text-center m-0 p-0">
+              <div class="col-sm-12 text-center m-0 p-1">
                 <?php render_post( $page, $index, $post->ID ); ?>
               </div>
             </div>
@@ -67,16 +69,44 @@
         </div>
         <?php } elseif ( $page === 1 && $index === 4 ) { ?>
           <div class="row">
-            <div class="col-sm-12 col-md-4 text-center p-0 pb-3 m-0 p-0">
+            <div class="col-sm-12 col-md-6 text-center p-1 m-0">
               <?php render_post( $page, $index, $post->ID ); ?>
             </div>
-        <?php } elseif ( $index === $wp_query->post_count ) { ?>
-            <div class="col-sm-12 col-md-4 text-center p-0 pb-3 m-0 p-0">
+        <?php } elseif ( $page === 1 && $index === 5 ) { ?>
+            <div class="col-sm-12 col-md-6 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index === 6 ) { ?>
+            <div class="col-sm-12 col-md-5 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index === 7 ) { ?>
+            <div class="col-sm-12 col-md-3 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index === $wp_query->post_count ) { ?>
+            <div class="col-sm-12 col-md-7 text-center p-1 m-0">
               <?php render_post( $page, $index, $post->ID ); ?>
             </div>
           </div>
+        <?php } elseif ( $page === 1 && $index === $wp_query->post_count - 3 ) { ?>
+            <div class="col-sm-12 col-md-6 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index === $wp_query->post_count - 2 ) { ?>
+            <div class="col-sm-12 col-md-6 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index === $wp_query->post_count - 1 ) { ?>
+            <div class="col-sm-12 col-md-5 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
+        <?php } elseif ( $page === 1 && $index < 8 ) { ?>
+            <div class="col-sm-12 col-md-6 text-center p-1 m-0">
+              <?php render_post( $page, $index, $post->ID ); ?>
+            </div>
         <?php } else { ?>
-          <div class="col-sm-12 col-md-4 text-center p-0 pb-3 m-0 p-0">
+          <div class="col-sm-12 col-md-4 text-center p-1 m-0">
             <?php render_post( $page, $index, $post->ID ); ?>
           </div>
         <?php } 
