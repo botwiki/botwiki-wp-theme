7cceda039a3e6f611962e2ba20f0a8e1ca370a32
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-07-27 11:56:21 -0400

    Updated 404 page design.

--- 404.php
+++ 404.php
@@ -5,9 +5,9 @@
   <main role="main" class="container-fluid m-0 p-0">
     <div class="container">
       <article id="post-404">
-        <h1><?php _e( 'Page not found', 'botwiki' ); ?></h1>
         <div class="row mt-5 mb-5">
           <div class="col-sm-12 col-md-6">
+            <h1><?php _e( 'Page not found', 'botwiki' ); ?></h1>
             <p>Sorry about that!</p>            
             <ul>
               <li>
@@ -29,6 +29,7 @@
           </div>
           <div class="col-sm-12 col-md-6">
             <p class="text-center">
+              <div id="robots" class="mt-5 text-center"></div>
               <!-- <img src="<?php echo get_template_directory_uri(); ?>/images/other/gear.jpg"> -->
             </p>
           </div>
