b85e8b6be3ba0dcd52f022ce08e319ca4a0e1904
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-07-29 10:55:18 -0400

    Make excerpt text bold.

--- single.php
+++ single.php
@@ -17,7 +17,7 @@
         <h1 class="text-center post-title mt-5"><?php the_title(); ?></h1>
         <!-- /post title -->
         <div class="post-content">
-          <p class="text-center lead mb-5 mt-n3"><?php echo get_the_excerpt(); ?></p>
+          <p class="text-center lead mb-5 mt-n3 font-weight-bold"><?php echo get_the_excerpt(); ?></p>
 
           <!-- post thumbnail -->
           <?php if ( has_post_thumbnail() ) {
