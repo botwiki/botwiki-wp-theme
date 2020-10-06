da6f165acd2389a0908f2b6937d4fe6eea819d11
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-10-06 11:47:32 -0400

    Lazy-load footer images.

--- footer.php
+++ footer.php
@@ -144,21 +144,21 @@
                   <div class="col-sm-6 col-md-3 mx-auto mt-5">
                     <p class="text-center">
                       <a href="https://fightfascism.glitch.me/">
-                        <img class="lazy-load" alt="Fight Fascism" title="Fight fascism! Sticker by Angus Johnston" src="<?php echo get_template_directory_uri(); ?>/images/other/fight-fascism.jpg" data-src="<?php echo get_template_directory_uri(); ?>/images/other/fight-fascism-block.jpg">
+                        <img loading="lazy" class="lazy-load" alt="Fight Fascism" title="Fight fascism! Sticker by Angus Johnston" src="<?php echo get_template_directory_uri(); ?>/images/other/fight-fascism.jpg" data-src="<?php echo get_template_directory_uri(); ?>/images/other/fight-fascism-block.jpg">
                       </a>
                     </p>              
                   </div>
                   <div class="col-sm-6 col-md-3 mx-auto mt-5">
                     <p class="text-center">
                       <a href="https://blacklivesmatter.com/">
-                        <img class="lazy-load" alt="Black Lives Matter" title="Black Lives Matter logo" src="<?php echo get_template_directory_uri(); ?>/images/other/blm.jpg" data-src="<?php echo get_template_directory_uri(); ?>/images/other/blm.jpg">
+                        <img loading="lazy" class="lazy-load" alt="Black Lives Matter" title="Black Lives Matter logo" src="<?php echo get_template_directory_uri(); ?>/images/other/blm.jpg" data-src="<?php echo get_template_directory_uri(); ?>/images/other/blm.jpg">
                       </a>
                     </p>              
                   </div>
                   <div class="col-sm-6 col-md-3 mx-auto mt-5">
                     <p class="text-center">
                       <a href="https://indieweb.org/">
-                        <img class="lazy-load" alt="IndieWeb" title="Support #indieweb!" src="<?php echo get_template_directory_uri(); ?>/images/other/indiewebcamp.png" data-src="<?php echo get_template_directory_uri(); ?>/images/other/indiewebcamp.png">
+                        <img loading="lazy" class="lazy-load" alt="IndieWeb" title="Support #indieweb!" src="<?php echo get_template_directory_uri(); ?>/images/other/indiewebcamp.png" data-src="<?php echo get_template_directory_uri(); ?>/images/other/indiewebcamp.png">
                       </a>
                     </p>              
                   </div>
