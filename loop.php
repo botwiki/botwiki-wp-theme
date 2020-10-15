eb02bbb9aa43b4969322d96c21db3b41bfeb8c8e
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-10-15 15:09:33 -0400

    Show featured image's dominant color as placeholders in the Latest from the blog section.

--- loop.php
+++ loop.php
@@ -1,35 +1,41 @@
 <div class="mt-5">
   <div class="row list">
-    <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+    <?php if ( have_posts() ): while ( have_posts() ) : the_post();
+      $post_id = get_the_ID();
+    ?>
       <div class="col-sm-12 col-md-6 col-lg-4 list-item">
 
         <div class="card mb-3 w-100">
         <!-- article -->
         <?php
           $is_external = false;
-          $link_url = get_post_meta( $post->ID, 'resource_url', true );
+          $link_url = get_post_meta( $post_id, 'resource_url', true );
           if ( empty( $link_url ) ){
-            $link_url = get_post_permalink( $post->ID );
+            $link_url = get_post_permalink( $post_id );
           }
           else{
             $is_external = true;
           }
 
-          if ( has_post_thumbnail() ){ ?>
-            <a href="<?php echo $link_url; ?>" title="<?php  get_the_title(); ?>">
-              <?php 
-                $post_thumbnail_url = get_the_post_thumbnail_url( $post->ID, 'medium' );
-                $post_thumbnail_url_full = get_the_post_thumbnail_url( $post->ID );
-              ?>
-              <img loading="lazy" class="lazy-load card-img-top" src="<?php echo $post_thumbnail_url; ?>" data-src="<?php echo $post_thumbnail_url_full;  ?>" alt="<?php echo $related_bot->post_title; ?>">
-            </a>
+          if ( has_post_thumbnail() ){
+            $post_thumbnail_url = get_the_post_thumbnail_url( $post_id, 'medium' );
+            $post_thumbnail_url_full = get_the_post_thumbnail_url( $post_id );
+
+            $dominant_color  = get_post_meta( $post_id, 'dominant_color', true );
+            $dominant_color_css = str_replace('[', 'background-color:rgb(', $dominant_color);
+            $dominant_color_css = str_replace(']', ')', $dominant_color_css);
+          ?>
+            <div style="<?php echo $dominant_color_css; ?>">
+              <a href="<?php echo $link_url; ?>" title="<?php  get_the_title(); ?>"> 
+                <img loading="lazy" class="lazy-load card-img-top" src="<?php echo $post_thumbnail_url; ?>" data-src="<?php echo $post_thumbnail_url_full;  ?>" alt="<?php echo $related_bot->post_title; ?>">
+              </a>
+            </div>
           <?php } ?>
           <div class="card-body" id="post-<?php the_ID(); ?>">
             <h5 class="card-title"><a href="<?php echo $link_url; ?>" title="<?php get_the_title(); ?>"><?php the_title(); ?></a></h5>
             <p class="card-text">   
             <?php
               bw_excerpt( 'html5wp_index' );
-              $post_id = get_the_ID();
               $post_type = get_post_type( $post_id );
               $post_meta = get_post_meta( $post_id );
             ?>
