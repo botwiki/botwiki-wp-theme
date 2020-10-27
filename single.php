89c57248c177cf9ec51e9a58b02da3640e3329e1
Stefan Bohacek <stefan.bohacek@gmail.com>
2020-10-27 17:09:52 -0400

    Cleanup.

--- single.php
+++ single.php
@@ -331,32 +331,30 @@
           <?php }
           ?></article><?php
           if ( $post_type === 'resource' && $prompt_donation ) { ?>
-            <div class="col-sm-12 col-md-12 col-lg-6 list-item">
-              <div class="container card mt-lg-4 mb-4 pl-0 pr-0">
-                <div class="card-body mt-4">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <h3 class="mt-0 mb-3 d-inline">Enjoyed the tutorial?</h3>
-                      <p class="mt-3"><strong>Consider supporting Botwiki!</strong></p>
-                      <ul class="btn-list mt-2 mb-2">
-                        <li>
-                          <a class="btn mb-2" title="Support via Patreon" rel="me" href="https://www.patreon.com/botwiki">Become a patron</a>
-                        </li>
-                        <li>
-                          <a class="btn mb-2" title="Donate via PayPal" rel="me" href="https://paypal.me/stefanbohacek">Donate</a>
-                        </li>
-                        <li> 
-                          <a class="btn mb-2" title="View list of supporters" href="https://botwiki.org/about/supporters/">See our supporters</a>
-                        </li> 
-                        <li>  
-                          <a class="btn mb-2" title="Twitter" rel="me" href="https://twitter.com/botwikidotorg">Follow @botwikidotorg</a> 
-                        </li> 
-                      </ul>
-                    </div>
+            <div class="container card mt-lg-4 mb-4 pl-0 pr-0">
+              <div class="card-body mt-4">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <h3 class="mt-0 mb-3 d-inline">Enjoyed the tutorial?</h3>
+                    <p class="mt-3"><strong>Consider supporting Botwiki!</strong></p>
+                    <ul class="btn-list mt-2 mb-2">
+                      <li>
+                        <a class="btn mb-2" title="Support via Patreon" rel="me" href="https://www.patreon.com/botwiki">Become a patron</a>
+                      </li>
+                      <li>
+                        <a class="btn mb-2" title="Donate via PayPal" rel="me" href="https://paypal.me/stefanbohacek">Donate</a>
+                      </li>
+                      <li> 
+                        <a class="btn mb-2" title="View list of supporters" href="https://botwiki.org/about/supporters/">See our supporters</a>
+                      </li> 
+                      <li>  
+                        <a class="btn mb-2" title="Twitter" rel="me" href="https://twitter.com/botwikidotorg">Follow @botwikidotorg</a> 
+                      </li> 
+                    </ul>
                   </div>
                 </div>
               </div>
-            </div>          
+            </div>
           <?php } ?>
         </div>
         <?php
