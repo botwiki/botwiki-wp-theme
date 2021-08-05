399c8f2f0fdf124cd250dd76e510b317c3cac17f
Stefan Bohacek <stefan.bohacek@gmail.com>
2021-08-05 15:37:02 -0400

    Temporary fix for code highlighting.

--- src/scripts/highlight-js.js
+++ src/scripts/highlight-js.js
@@ -1,13 +1,21 @@
-$( function(){
-  'use strict';
-  if ( typeof hljs !== 'undefined' ){
-    hljs.initHighlighting.called = false;
-    hljs.initHighlighting();
+$( function() {
+  $( document ).ready( function( $ ){
+    function highlightCode(){
+        document.querySelectorAll( 'pre code' ).forEach( ( block ) => {
+            hljs.highlightBlock( block );
+        } );
 
-    let blocks = document.querySelectorAll('pre code.hljs');
-        Array.prototype.forEach.call(blocks, function(block) {
-        const language = block.result.language;
-        block.insertAdjacentHTML( 'afterbegin', `<label>${language}</label>` );
-    });        
-  }
+        let blocks = document.querySelectorAll('pre code.hljs');
+            Array.prototype.forEach.call(blocks, function(block) {
+            const language = block.result.language;
+            block.insertAdjacentHTML( 'afterbegin', `<label>${language}</label>` );
+        });        
+    }
+
+    if ( typeof hljs !== 'undefined' ){
+        highlightCode();
+    } else {
+        setTimeout( highlightCode, 2000 );
+    }
+  } );
 } );
