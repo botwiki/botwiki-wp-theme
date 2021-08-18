7ed2808a97372f7277635eea2ff0476b57fa38d1
Stefan Bohacek <stefan.bohacek@gmail.com>
2021-08-18 14:15:32 -0400

    Cleanup.

--- src/scripts/form-submissions.js
+++ src/scripts/form-submissions.js
@@ -1,16 +1,19 @@
-$ = jQuery.noConflict(true);
-
 $(function() {
-  let $forms = $( 'form' );
+  let $forms = $( 'form[id^="submit-"' );
   if ( $forms && $forms.length ){
+    window.formWasEdited = false;
     window.isSubmittingForm = false;
+
+    $forms.find( 'input' ).blur(function(){
+      window.formWasEdited = true;
+    } );
 
     $forms.submit( function( ev ) {
       window.isSubmittingForm = true;
     } );
 
     $( window ).bind( 'beforeunload', function(){
-      if ( !window.isSubmittingForm ){
+      if ( window.formWasEdited && !window.isSubmittingForm ){
         return 'Are you sure you want to leave?';
       }
     } );
