fff7b8ce32b1658eb3757ce97a19f793dc30105a
Stefan Bohacek <stefan.bohacek@gmail.com>
2021-08-18 13:47:21 -0400

    Warn when leaving the bot/resource submission pages.

--- src/scripts/submit-your-bot.js
+++ src/scripts/submit-your-bot.js
@@ -37,6 +37,10 @@ $( function() {
 
   if ( $body.hasClass( 'page-template-template-submit-your-bot' ) ){
 
+    $( window ).bind( 'beforeunload', function(){
+      return 'Are you sure you want to leave?';
+    } );
+
     /* Disassociate bot author to allow logged in users to submit other people's bots. */
 
     let $disassociateAuthorBtn = $( '#disassociate-author' );
