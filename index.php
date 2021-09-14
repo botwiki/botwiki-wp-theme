6a2af39d7d616dbabaaae67966382f13c6b20b56
Stefan Bohacek <stefan.bohacek@gmail.com>
2021-09-14 14:45:14 -0400

    Cleanup.

--- index.php
+++ index.php
@@ -26,14 +26,16 @@
       </h1> -->
 			<?php get_template_part('loop-blog'); ?>
 			<?php get_template_part('pagination'); ?>
-      <h3>Wait, there's more!</h3>
-      <ul>
-        <li>follow us at <a href="https://twitter.com/botwikidotorg">@botwikidotorg</a></li>
-        <li>keep up with new bots and blog updates via <a href="https://twitter.com/newonbotwiki">@newonbotwiki</a></li>
-        <li>subscribe to our <a href="/botwiki-weekly-digest/">weekly digest</a></li>
-        <li>check out our <a href="/learn/#blogs-and-websites">blogroll</a></li>
-        <li><a href="/about/team">meet the team</a></li>
-      </ul>
+      <div class="blog-more">
+        <h3>Wait, there's more!</h3>
+        <ul>
+          <li>follow us at <a href="https://twitter.com/botwikidotorg">@botwikidotorg</a></li>
+          <li>keep up with new bots and blog updates via <a href="https://twitter.com/newonbotwiki">@newonbotwiki</a></li>
+          <li>subscribe to our <a href="/botwiki-weekly-digest/">weekly digest</a></li>
+          <li>check out our <a href="/learn/#blogs-and-websites">blogroll</a></li>
+          <li><a href="/about/team">meet the team</a></li>
+        </ul>
+      </div>
 		</div>
 	</main>
 <?php get_footer(); ?>
