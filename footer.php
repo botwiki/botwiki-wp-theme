4e66a52dc2b61d8064e69b5978004313db7c5710
Stefan Bohacek <stefan@fourtonfish.com>
2022-08-01 09:07:38 -0400

    Removed link to botzine.org from the site footer.

--- footer.php
+++ footer.php
@@ -126,7 +126,7 @@
           <div class="container">
             <div class="row">
               <div class="col-12 pt-3 pb-5">
-                <p>This website is <a href="https://github.com/botwiki/botwiki-wp-theme">open source</a> and powered by <a href="https://wordpress.org" title="WordPress">WordPress</a>. Botwiki and the <a href="http://botzine.org/">Bot! zine</a> and <a href="https://botmakers.org/">Botmakers</a> landing pages are all proudly hosted by <a href="https://digitalocean.com/"><img class="footer-logo-do" src="<?php echo get_template_directory_uri(); ?>/images/logos/digitalocean.svg"></a>, a generous supporter and the sponsor of the very first <a href="<?php echo site_url(); ?>/monthly-bot-challenge/">Monthly Bot Challenge</a>.</p>
+                <p>This website is <a href="https://github.com/botwiki/botwiki-wp-theme">open source</a> and powered by <a href="https://wordpress.org" title="WordPress">WordPress</a>. Botwiki and <a href="https://botmakers.org/">Botmakers</a> landing pages are all proudly hosted by <a href="https://digitalocean.com/"><img class="footer-logo-do" src="<?php echo get_template_directory_uri(); ?>/images/logos/digitalocean.svg"></a>, a generous supporter and the sponsor of the very first <a href="<?php echo site_url(); ?>/monthly-bot-challenge/">Monthly Bot Challenge</a>.</p>
                 <p>One more way to support us is to sign up using our <a href="https://www.digitalocean.com/?refcode=9e279abc3337">DigitalOcean referral link</a>, which will also earn you a $10 starter credit.</p>
                 <p>For attributions, see the <a href="<?php echo site_url(); ?>/about/attributions">Attributions page</a>.</p>
               </div>
