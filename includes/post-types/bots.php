368a678e21cb6bb13792db189b0cb7b82386df98
Stefan Bohacek <stefan@fourtonfish.com>
2022-11-28 01:07:10 -0500

    Added support for fedibot.dev embeds.

--- includes/post-types/bots.php
+++ includes/post-types/bots.php
@@ -700,6 +700,7 @@ HTML;
             }
             elseif ( strpos( $tweet_url, 'botsin.space/' ) !== false ||
                      strpos( $tweet_url, 'beeping.town/' ) !== false ||
+                     strpos( $tweet_url, 'fedibot.dev/' ) !== false ||
                      strpos( $tweet_url, 'mastodon.social/' ) !== false ) {
               $bot_tweets_html .= '<blockquote><iframe src="' . $tweet_url . '/embed" class="mastodon-embed" style="max-width: 100%; border: 0" width="400"></iframe></blockquote>';
             }
